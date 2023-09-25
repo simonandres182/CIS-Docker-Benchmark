bfa941127792b604dc3f83a9b77b8df0acba6612
Simon Benitez <simonandres182@gmail.com>
2023-09-25 15:05:58 -0400

    estructura inicial de la web-app

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template('index.html')
+
+@app.route("/pruebas")
+def pruebas():
+    return render_template('pruebas.html')
+
 
 
 if __name__ == '__main__':
