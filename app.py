965b1d27b80124532fe3683d31986970d8e4baea
Simon Benitez <simonandres182@gmail.com>
2023-10-02 14:42:21 -0300

    se incorporan  nuevas pruebas

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask import render_template, request
 import sqlite3
+import subprocess
 import time
+import os
 
 app = Flask(__name__)
 
@@ -9,6 +11,14 @@ def get_db_connection():
     conn = sqlite3.connect('app.db')
     conn.row_factory = sqlite3.Row
     return conn
+
+def registrar_resultado(prueba_id, resultado, msg):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("""UPDATE pruebas SET pass='%s', result='%s' WHERE id=%s""" % (resultado, msg, prueba_id))
+    conn.commit()
+    cur.close()
+    conn.close()
 
 @app.route("/")
 def index():
@@ -24,8 +34,58 @@ def pruebas():
     else:
         data = request.json
         prueba_id = data.get('prueba_id')
-        time.sleep(3)
-        return {'id':prueba_id}
+        if (prueba_id == 1):
+            result = subprocess.run(['mountpoint', '/var/lib/docker'], stdout=subprocess.PIPE)
+            respuesta = result.stdout.decode('utf-8').replace('\n','')
+            time.sleep(1) #damos tiempo para capturas la salida
+            if "no" in respuesta:
+                registrar_resultado(prueba_id, "N", respuesta)
+                return {'prueba':'fail'}
+            else:
+                registrar_resultado(prueba_id, "S", respuesta)
+                return {'prueba':'pass'}
+        
+        elif (prueba_id == 2):
+            comando = "stat -c %U:%G /usr/lib/systemd/system/docker.service | grep -v root:root"
+            resultado = subprocess.getoutput(comando)
+            lineas = resultado.split('\n')
+            primera_linea = lineas[0]
+            time.sleep(1) #damos tiempo para capturas la salida
+            if primera_linea == "":
+                registrar_resultado(prueba_id, "S", primera_linea)
+                return {'prueba':'pass'}
+            else:
+                registrar_resultado(prueba_id, "N", primera_linea)
+                return {'prueba':'fail'}
+            
+        elif (prueba_id == 3):
+            comando = "stat -c %a /usr/lib/systemd/system/docker.socket"
+            resultado = subprocess.getoutput(comando)
+            lineas = resultado.split('\n')
+            primera_linea = lineas[0]
+            time.sleep(1) #damos tiempo para capturas la salida
+            if primera_linea == "644":
+                registrar_resultado(prueba_id, "S", primera_linea)
+                return {'prueba':'pass'}
+            else:
+                registrar_resultado(prueba_id, "N", primera_linea)
+                return {'prueba':'fail'}
+            
+        elif (prueba_id == 4):
+            comando = "stat -c %U:%G /etc/docker | grep -v root:root"
+            resultado = subprocess.getoutput(comando)
+            lineas = resultado.split('\n')
+            primera_linea = lineas[0]
+            time.sleep(1) #damos tiempo para capturas la salida
+            if primera_linea == "":
+                registrar_resultado(prueba_id, "S", primera_linea)
+                return {'prueba':'pass'}
+            else:
+                registrar_resultado(prueba_id, "N", primera_linea)
+                return {'prueba':'fail'}
+            
+        else:
+            return {'prueba':''}
 
 
 
--- static/js/pruebas.js
+++ static/js/pruebas.js
@@ -8,7 +8,13 @@ $(document).ajaxStop($.unblockUI);
 // -----------------------------------------------------------------------
 $(document).ready(function () {
     $("#pass-1").hide();
+    $("#fail-1").hide();
     $("#pass-2").hide();
+    $("#fail-2").hide();
+    $("#pass-3").hide();
+    $("#fail-3").hide();
+    $("#pass-4").hide();
+    $("#fail-4").hide();
 });
 
 
@@ -20,7 +26,13 @@ $("#probar-1").on("click", function () {
         type: 'POST',
         url: './pruebas',
         data: '{"prueba_id":1}',
-        success: function (data) {console.log(data.id)},
+        success: function (data) {
+            if (data.prueba == 'pass') {
+                $("#pass-1").show();
+            } else {
+                $("#fail-1").show();
+            }
+        },
         contentType: "application/json",
         dataType: 'json'
     });
@@ -32,7 +44,49 @@ $("#probar-2").on("click", function () {
         type: 'POST',
         url: './pruebas',
         data: '{"prueba_id":2}',
-        success: function (data) {console.log(data.id)},
+        success: function (data) {
+            if (data.prueba == 'pass') {
+                $("#pass-2").show();
+            } else {
+                $("#fail-2").show();
+            }
+        },
+        contentType: "application/json",
+        dataType: 'json'
+    });
+});
+
+$("#probar-3").on("click", function () {
+    $.blockUI({ message: '<h1><img src="https://tamilnaducouncil.ac.in/wp-content/uploads/2018/10/loading-gif.gif" /> Ejecuntando Prueba..</h1>' });
+    $.ajax({
+        type: 'POST',
+        url: './pruebas',
+        data: '{"prueba_id":3}',
+        success: function (data) {
+            if (data.prueba == 'pass') {
+                $("#pass-3").show();
+            } else {
+                $("#fail-3").show();
+            }
+        },
+        contentType: "application/json",
+        dataType: 'json'
+    });
+});
+
+$("#probar-4").on("click", function () {
+    $.blockUI({ message: '<h1><img src="https://tamilnaducouncil.ac.in/wp-content/uploads/2018/10/loading-gif.gif" /> Ejecuntando Prueba..</h1>' });
+    $.ajax({
+        type: 'POST',
+        url: './pruebas',
+        data: '{"prueba_id":4}',
+        success: function (data) {
+            if (data.prueba == 'pass') {
+                $("#pass-4").show();
+            } else {
+                $("#fail-4").show();
+            }
+        },
         contentType: "application/json",
         dataType: 'json'
     });
